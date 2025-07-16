49e95bf2f66ff7c6dbaebe5a6c8ddf689213ba30
June Rhodes <jrhodes@redpoint.games>
2025-07-16 14:20:48 +1000

    Improve next steps page

--- src/Button.tsx
+++ src/Button.tsx
@@ -4,9 +4,13 @@ export default function Button(props: {
   href: string;
   children?: React.ReactNode;
   text?: string;
+  type?: string;
 }) {
   return (
-    <Link to={props.href} className="button button--primary">
+    <Link
+      to={props.href}
+      className={`button button--${props.type ?? "primary"}`}
+    >
       {props.text ?? props.children}
     </Link>
   );
